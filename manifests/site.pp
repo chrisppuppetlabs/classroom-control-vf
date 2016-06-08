6e37da3309c283d982ccf045c1bbe48ab89812cd
chrisppuppetlabs <chrisp@ccbilleu.com>
2016-06-08 11:21:35 +0200

    Creating /etc/motd via an exec
    Creating /etc/motd via an exec cowsay only if it does not exist

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,11 +44,16 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  file { "/etc/motd":
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => 'This content is populated via a Puppet File Resource'
+  #file { "/etc/motd":
+  #  ensure  => file,
+  #  owner   => 'root',
+  #  group   => 'root',
+  #  mode    => '0644',
+  #  content => 'This content is populated via a Puppet File Resource'
+  #}
+  
+  exec { "cowsay 'Welcome to ${::fqdn}! > /etc/motd":
+    path    => 'usr/local/bin',
+    creates => '/etc/motd'
   }
 }
