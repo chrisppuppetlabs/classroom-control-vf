002a1bcaaf17db31985ed77c4e607c8d7cf78536
chrisppuppetlabs <chrisp@ccbilleu.com>
2016-06-08 11:26:10 +0200

    fix to exec

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,7 +52,7 @@ node default {
   #  content => 'This content is populated via a Puppet File Resource'
   #}
   
-  exec { "cowsay 'Welcome to ${::fqdn}! > /etc/motd":
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     path    => '/usr/local/bin',
     creates => '/etc/motd'
   }
